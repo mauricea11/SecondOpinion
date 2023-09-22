e08b0a39f947fd20c6db197e64f7c3602ff86a23
mauricea11 <mauriceaugust117@gmail.com>
2023-09-22 01:05:03 -0400

    Added Google search method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import PyPDF2
+import psycopg2
+import requests
 
 def getTextFromPDF(pdffile):
     with open(pdffile, 'rb') as pdf:
@@ -12,7 +14,21 @@ def getTextFromPDF(pdffile):
 
         return text
 
-extractedText =  getTextFromPDF("PS2356019869693 copy.pdf")
+def printText():
+    extractedText =  getTextFromPDF("PS2356019869693 copy.pdf")
+    for text in extractedText:
+        print(text)
 
-for text in extractedText:
-    print(text)
+def postgresConnect():
+    connection = psycopg2.connect(host='localhost', user='mauriceaugust', password='Destiny4days', dbname='postgres', port='5432')
+    cursor = connection.cursor()
+    print(connection)
+
+def makeHttpRequest():
+    API_KEY = 'AIzaSyD1Y3hqtMOwKBtoQSltogaN6cFEzlyff6o'
+    ENGINE_ID = '26d42802da9b540bb'
+    response = requests.get('https://www.googleapis.com/customsearch/v1?key='+API_KEY+'&cx='+ENGINE_ID+'&q=lectures')
+
+    printText()
+    
+makeHttpRequest()
